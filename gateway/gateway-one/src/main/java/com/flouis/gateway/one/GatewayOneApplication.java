fafbf24f66edd84b78e60451f42ab09c92ace535
Floui2017 <315587951@qq.com>
2020-07-02 17:52:26 +0800

    springcloud gateway Path route

---
+++ gateway/gateway-one/src/main/java/com/flouis/gateway/one/GatewayOneApplication.java
@@ -0,0 +1,13 @@
+package com.flouis.gateway.one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayOneApplication.class, args);
+	}
+
+}
